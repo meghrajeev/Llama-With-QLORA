7478212f5ea7d92f1f554fbf84b03f9bc7bc76e3
meghrajeev <mrajeev@andrew.cmu.edu>
2023-10-29 23:43:47 +0000

    data_preprocessing script

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,35 +1,93 @@
 import re
-import os
+import json
+import os 
+import sys
 
-def clean_conversations(filename, output_dir):
+def remove_placeholders(message):
+	skip_list = ['You blocked this user.','You unblocked this user.','<This message was edited>','This message was deleted.',' omitted\n']
+	for phrase in skip_list:
+		if phrase in message:
+			return True
+	return False
 
-    date_pattern = re.compile(r'\[\d{1,2}\/\d{1,2}\/\d{2},\s\d{1,2}:\d{2}:\d{2}\s[APMapm]{2}\]')
+def replace_users(message, contact_name, friend_name,bot_name, your_contact_name):
+	message = re.sub(your_contact_name, bot_name, message)
+	message = re.sub(contact_name, friend_name, message)
+	return message
 
-    with open(filename, 'r', encoding='utf-8') as file:
-        contents = file.read()
-    
-    cleaned_contents = date_pattern.sub('', contents)
-    
-    os.makedirs(output_dir, exist_ok=True)
-    
-    base_name = os.path.basename(filename)
-    cleaned_base_name = 'cleaned' + base_name
-    output_filename = os.path.join(output_dir, cleaned_base_name)
-    
-    with open(output_filename, 'w', encoding='utf-8') as file:
-        file.write(cleaned_contents)
+def remove_links(message):
+	message = re.sub(r"http\S*", '', message)
+	return message
 
-def main():
-    input_dir = 'data/raw_data'
-    output_dir = 'data/preprocessed_data'
-    
-    for filename in os.listdir(input_dir):
-        if filename.endswith('.txt'):
-            full_path = os.path.join(input_dir, filename)
+def get_user_text(message):
+	if ': ' not in message:
+		return None,message
+	try:
+		user, text = message.split(": ", 1)
+	except Exception as e:
+		print(e)
+		print(message)
+		return message.split(":")[0],''
+	return user, text
 
-            clean_conversations(full_path, output_dir)
+def clean_text(text):
+	return text.split(":")[0]
+
+def collate_messages(messages, user_name, bot_name, friend_name):
+	conversations = []
+
+	fp = 0
+	sp = 0
+	snippet = ''
+
+	while sp < len(messages):
+		if snippet =='':
+			og_user,_ = get_user_text(messages[fp])
+		cur_user,text = get_user_text(messages[sp])
+		if cur_user==og_user or cur_user==None:
+			snippet = snippet + clean_text(text)
+			sp+=1
+		else:
+			if og_user==user_name:
+				conversations.append({'Friend (' + friend_name +')': snippet})
+			if og_user==bot_name:
+				conversations.append({og_user: snippet})
+			snippet=''
+			fp = sp
+
+	#Append last conversation
+	conversations.append({og_user:snippet})
+	return conversations
+
 
 if __name__ == "__main__":
-    main()
+	if len(sys.argv) != 7:
+		print("Usage: preprocessing.py <your_name> <your_contact_name> <friend_name> <friend_contact_name> <input_folder_path> <output_folder_path>")
+		sys.exit(1)
 
+	bot_name = sys.argv[1]
+	your_contact_name = sys.argv[2]
+	friend_name = sys.argv[3]
+	contact_name = sys.argv[4]
+	input_folder_path = sys.argv[5]
+	output_folder_path = sys.argv[6]
 
+	with open(input_folder_path+'/'+friend_name+'Chat.txt', encoding="utf-8") as f:
+		lines = f.readlines()
+
+	regex = r"\s?\[\d{1,2}\/\d{1,2}\/\d{2,4}\, \d{1,2}:\d{1,2}:\d{1,2}\s[APM]{2}\]\s" #remove timestamps
+
+	dataset = []
+
+	for line in lines:
+		message = re.sub(regex, "", line)
+		if remove_placeholders(message):
+			continue
+		message = remove_links(message)
+		message = replace_users(message, contact_name, friend_name, bot_name, your_contact_name)
+
+		dataset.append(message)
+
+	dataset = collate_messages(dataset, friend_name, bot_name, friend_name)
+	with open(output_folder_path+'/'+friend_name+'Chat.json', 'w') as file:
+		json.dump(dataset, file)
